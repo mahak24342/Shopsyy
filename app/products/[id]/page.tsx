309eb9e481fadf808cc80a4434be231474f1175f
mahak porwal <porwalmahak784@gmail.com>
2025-05-09 18:08:15 +0530

    Initial commit

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,21 +1,17 @@
-import Details from '@/app/components/Details'
+import Details from '@/app/components/Details';
 import { stripe } from '@/lib/stripe';
-import React from 'react'
-//import stripe from 'stripe';
-
-const page = async(
-    {params,}:{
-        params:{id:string};
-    }
-) => {
-    const product=await stripe.products.retrieve(params.id,{
-        expand:["default_price"]
+import React from 'react';
+export default async function Page({
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  }) {
+    const { id } = await params;
+    const product = await stripe.products.retrieve(id, {
+      expand: ["default_price"],
     });
-
-    const plainProduct=JSON.parse(JSON.stringify(product));
-  return (
-   <Details product={plainProduct}/>
-  )
-}
-
-export default page
+  
+    const plainProduct = JSON.parse(JSON.stringify(product));
+    return <Details product={plainProduct} />;
+  }
+  
